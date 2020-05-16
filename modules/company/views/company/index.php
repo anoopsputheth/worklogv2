6e9cd2bcb30b77fcb7ab1e810668799286b7a3c1
Anoop PS <anoopsputheth@gmail.com>
2020-05-16 08:01:34 +0530

    create company modal

--- modules/company/views/company/index.php
+++ modules/company/views/company/index.php
@@ -2,6 +2,72 @@
 
 use yii\helpers\Html;
 use yii\grid\GridView;
+use yii\widgets\ActiveForm;
+
+use app\modules\company\models\Company;
+
+?>
+
+
+
+
+<?php
+
+   yii\bootstrap\Modal::begin([
+
+            'header'=>'<h4>Create Company</h4>',
+            'id'=>'modal_create_company',
+            'size'=>'modal-lg',
+   ]);
+
+?>
+
+
+<div class="company-form">
+
+    <?php $form = ActiveForm::begin([ 'enableAjaxValidation' => true, 'action' => ['create'] ]);  
+
+     $model = new Company(); ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'contact_person')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'address')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'phone')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'fax')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'zip')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'city')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'state')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
+
+    
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
+
+
+<?php
+
+ yii\bootstrap\Modal::end();
+
+?>
+
+
+<?php
 
 /* @var $this yii\web\View */
 /* @var $searchModel app\modules\company\models\CompanySearch */
@@ -15,7 +81,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Create Company', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Create Company', ['create'], ['class' => 'btn btn-success', 'id' => 'create_company']) ?>
     </p>
 
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
@@ -46,3 +112,28 @@ $this->params['breadcrumbs'][] = $this->title;
 
 
 </div>
+
+
+
+<?php
+
+    $this->registerJs(
+
+        '$("document").ready(function(){ 
+
+
+            $("#create_company").click(function(event){   
+
+               event.preventDefault();
+
+               $("#modal_create_company").modal("show");
+
+            });
+
+
+             
+        });'
+    );
+
+
+?>
